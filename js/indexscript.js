b9e55cb7d42c1fe97296a6fe6ba47434190a119e
srubinic <31941197+srubinic@users.noreply.github.com>
2018-01-15 15:00:03 -0800

    Add files via upload

---
+++ js/indexscript.js
@@ -0,0 +1,21 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('#headrow', {duration:200});
+
+sr.reveal('#row2', {
+    origin: 'left',
+    duration: 2000
+});
+
+
+sr.reveal('#row3', { 
+  duration: 1000
+});
+
+sr.reveal('#row4', { 
+ duration: 1000
+});
+
+sr.reveal('#row5', { 
+  duration: 1000
+});
